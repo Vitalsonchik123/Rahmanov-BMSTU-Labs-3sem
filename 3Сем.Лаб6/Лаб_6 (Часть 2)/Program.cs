7cfb433d36ce4cabce4eda14364f60674c3a8377
Vitalsonchik123 <Vitaliir2008@mail.ru>
2025-12-21 17:04:10 +0300

    Add files via upload

---
+++ 3Сем.Лаб6/Лаб_6 (Часть 2)/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Reflection;
+
+//Класс атрибута
+[AttributeUsage(AttributeTargets.Property)]
+public class DisplayAttribute : Attribute
+{
+    public string Label { get; }
+
+    public DisplayAttribute(string label)
+    {
+        Label = label;
+    }
+}
+
+//Класс, содержащий конструкторы, свойства, методы
+public class Person
+{
+    [Display("Полное имя")] 
+    public string FullName { get; set; }
+
+    [Display("Возраст")]
+    public int Age { get; set; }
+
+    public string Email { get; set; } // Свойство без атрибута
+
+    public Person() { }
+
+    public Person(string fullName, int age)
+    {
+        FullName = fullName;
+        Age = age;
+    }
+
+    public Person(string fullName, int age, string email) : this(fullName, age)
+    {
+        Email = email;
+    }
+
+    // Методы
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Имя: {FullName}, Возраст: {Age}");
+    }
+
+    public string GetGreeting(string prefix)
+    {
+        return $"{prefix}, {FullName}!";
+    }
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine("Это приватный метод");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Type personType = typeof(Person);
+
+        Console.WriteLine("Информация о классе Person");
+        Console.WriteLine($"Имя класса: {personType.Name}");
+        Console.WriteLine($"Пространство имен: {personType.Namespace}");
+        Console.WriteLine();
+
+        Console.WriteLine("Конструкторы");
+        ConstructorInfo[] constructors = personType.GetConstructors();
+        foreach (var constructor in constructors)
+        {
+            Console.Write($"{constructor.Name}(");
+            ParameterInfo[] parameters = constructor.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Console.Write($"{parameters[i].ParameterType.Name} {parameters[i].Name}");
+                if (i < parameters.Length - 1) Console.Write(", ");
+            }
+            Console.WriteLine(")");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Свойства");
+        PropertyInfo[] properties = personType.GetProperties();
+        foreach (var property in properties)
+        {
+            Console.WriteLine($"- {property.PropertyType.Name} {property.Name} " +
+                            $"{{ get; set; }}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Публичные методы");
+        MethodInfo[] methods = personType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+        foreach (var method in methods)
+        {
+            if (method.IsSpecialName) continue;
+
+            Console.Write($"{method.ReturnType.Name} {method.Name}(");
+            ParameterInfo[] parameters = method.GetParameters();
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                Console.Write($"{parameters[i].ParameterType.Name} {parameters[i].Name}");
+                if (i < parameters.Length - 1) Console.Write(", ");
+            }
+            Console.WriteLine(")");
+        }
+        Console.WriteLine();
+
+        //только те свойства, которым назначен атрибут
+        Console.WriteLine("Свойства с атрибутом Display");
+        foreach (var property in properties)
+        {
+            var attribute = property.GetCustomAttribute<DisplayAttribute>();
+            if (attribute != null)
+            {
+                Console.WriteLine($"{property.Name} -> {attribute.Label}");
+            }
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Вызов метода с помощью рефлексии");
+
+        Person person = new Person("Иван Иванов", 30, "ivan@example.com");
+
+        MethodInfo getGreetingMethod = personType.GetMethod("GetGreeting");
+
+        if (getGreetingMethod != null)
+        {
+            object result = getGreetingMethod.Invoke(person, new object[] { "Привет" });
+            Console.WriteLine($"Результат вызова метода: {result}");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Вызов метода DisplayInfo");
+        MethodInfo displayInfoMethod = personType.GetMethod("DisplayInfo");
+        if (displayInfoMethod != null)
+        {
+            displayInfoMethod.Invoke(person, null);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
---
+++ 3Сем.Лаб6/Лаб_6/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Lab6_Delegates
+{
+    class Program
+    {
+        // 2 определение делегата, принимающего несколько параметров различных типов и возвращающего значение произвольного типа
+        delegate double OperationDelegate(int a, double b, string operation);
+
+        // 3 метод данного делегата
+        static double PerformOperation(int a, double b, string operation)
+        {
+            Console.WriteLine($"Выполняется операция: {a} {operation} {b}");
+
+            switch (operation)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    if (b != 0)
+                        return a / b;
+                    else
+                        throw new DivideByZeroException("Деление на ноль!");
+                case "^":
+                    return Math.Pow(a, b);
+                default:
+                    throw new ArgumentException($"Неизвестная операция: {operation}");
+            }
+        }
+
+        // 4 принимает разработанный делегат в качестве одного из входных параметров
+        static void ProcessNumbers(int x, double y, string op, OperationDelegate operationDelegate)
+        {
+            Console.WriteLine($"\nОбработка чисел: {x}, {y}");
+            Console.WriteLine($"Операция: {op}");
+
+            try
+            {
+                double result = operationDelegate(x, y, op);
+                Console.WriteLine($"Результат: {result:F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        //5 обобщенный делегат Func<>
+        static void ProcessNumbersWithFunc(int x, double y, string op, Func<int, double, string, double> operationFunc)
+        {
+            Console.WriteLine($"\nОбработка с использованием Func<>: {x}, {y}");
+            Console.WriteLine($"Операция: {op}");
+
+            try
+            {
+                double result = operationFunc(x, y, op);
+                Console.WriteLine($"Результат: {result:F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // 4. Вызов метода, передавая в качестве параметра-делегата:
+
+            // а) метод, разработанный в пункте 3
+            Console.WriteLine("\nКастомный делегатт с именованным методом");
+            OperationDelegate delegate1 = PerformOperation;
+            ProcessNumbers(10, 3.5, "+", delegate1);
+            ProcessNumbers(15, 4.0, "*", delegate1);
+            ProcessNumbers(20, 5.0, "/", delegate1);
+
+            // б) лямбда-выражение
+            Console.WriteLine("\nКастомный делегат с лямбда-выражением");
+            OperationDelegate delegate2 = (int a, double b, string op) =>
+            {
+                Console.WriteLine($"Лямбда: {a} {op} {b}");
+                return op switch
+                {
+                    "+" => a + b,
+                    "-" => a - b,
+                    "*" => a * b,
+                    "/" => b != 0 ? a / b : throw new DivideByZeroException("Деление на ноль в лямбде!"),
+                    "^" => Math.Pow(a, b),
+                    _ => throw new ArgumentException($"Неизвестная операция в лямбде: {op}")
+                };
+            };
+
+            ProcessNumbers(8, 2.0, "-", delegate2);
+            ProcessNumbers(5, 2.0, "^", delegate2);
+
+            // 5. Использование обобщенного делегата Func<>
+            Console.WriteLine("\nОбобщенного делегат Func<>");
+
+            // а) с именованным методом
+            ProcessNumbersWithFunc(12, 6.0, "/", PerformOperation);
+
+            // б) с лямбда-выражением
+            ProcessNumbersWithFunc(7, 3.0, "*", (a, b, op) =>
+            {
+                Console.WriteLine($"Func-лямбда: {a} {op} {b}");
+                return a * b;
+            });
+
+            // в) с разными операциями через Func<>
+            Func<int, double, string, double> powerFunc = (a, b, op) => Math.Pow(a, b);
+            ProcessNumbersWithFunc(2, 8.0, "^", powerFunc);
+
+            // Проверка ошибок
+            Console.WriteLine("\nПроверка обработки ошибок");
+            ProcessNumbers(10, 0, "/", PerformOperation);
+            ProcessNumbersWithFunc(10, 0, "/", PerformOperation);
+
+            Console.WriteLine("\nПрограмма завершена. Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
